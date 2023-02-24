f445c17c2aba9d0df40f5d438e46b38169ddcd73
MarcosCamara01 <115667807+MarcosCamara01@users.noreply.github.com>
2023-02-24 19:59:26 +0100

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <div className='footer-bx'><a href="https://portfoliomarcos.com/">Created by Marcos Cámara</a></div>
-  )
-}
--- src/helpers/Global.jsx
+++
@@ -1,3 +0,0 @@
-export const Global = {
-  url: "https://todo-app-server-production-dff2.up.railway.app/api/"
-}
